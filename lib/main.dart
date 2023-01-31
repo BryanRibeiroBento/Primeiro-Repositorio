efc90b30d057eba3216a2ea7542ae5a62883134b
Bryan Ribeiro Bento <bryan@MacBook-Air-de-Bryan.local>
2023-01-31 17:24:00 -0300

    outro commit do mesmo teste

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Testando conflito:',
+              'O que eu quiser',
             ),
             Text(
               '$_counter',
